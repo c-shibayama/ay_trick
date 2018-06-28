2f68da8bbea52242bd4133b9ca8bd48efbb4509b
Akihiko Yamaguchi <info@akihikoy.net>
2018-06-28 11:28:24 +0900

    Added a low-level implementation of velocity control of UR:   A direct connection to UR over Ethernet is used (no ROS topic).   Implemented in ay_skill/ur/velctrl.py.   The old ROS-topic-based code is moved to ur/velctrl_ros.py.   Note: The low-level velocity control implementation of UR is general purpose.   They will be moved to ay_py in future.

--- ay_skill/ur/velctrl.py
+++ ay_skill/ur/velctrl.py
@@ -14,6 +14,128 @@ def Help():
 def Run(ct,*args):
   print 'Error:',Help()
 
+
+'''
+A low-level velocity control implementation of UR
+with a direct connection to UR over Ethernet (no ROS topic is used).
+FIXME:TODO: This is an experimental code.
+The implementation of TUR* are general-purpose classes.
+--> Move these codes to ay_py.misc.
+'''
+
+import socket,struct
+
+class TURVersion(object):
+  def __init__(self):
+    self.timestamp= 0  #uint64_t
+    self.source= 0  #int8_t
+    self.robot_message_type= 0  #int8_t
+    self.project_name_size= 0  #int8_t
+    self.project_name= ''  #char[15]
+    self.major_version= 0  #int8_t
+    self.minor_version= 0  #int8_t
+    self.svn_revision= 0  #int
+    self.build_date= ''  #char[25]
+
+class TURState(object):
+  ROBOT_STATE= 16
+  ROBOT_MESSAGE= 20
+  PROGRAM_STATE_MESSAGE= 25
+  ROBOT_MESSAGE_TEXT= 0
+  ROBOT_MESSAGE_PROGRAM_LABEL= 1
+  PROGRAM_STATE_MESSAGE_VARIABLE_UPDATE= 2
+  ROBOT_MESSAGE_VERSION= 3
+  ROBOT_MESSAGE_SAFETY_MODE= 5
+  ROBOT_MESSAGE_ERROR_CODE= 6
+  ROBOT_MESSAGE_KEY= 7
+  ROBOT_MESSAGE_REQUEST_VALUE= 9
+  ROBOT_MESSAGE_RUNTIME_EXCEPTION= 10
+
+  def __init__(self):
+    self.version= TURVersion()
+
+  def GetVersion(self):
+    return self.version.major_version + 0.1*self.version.minor_version + 0.0000001*self.version.svn_revision
+
+  def Unpack(self, buf):
+    offset= 0
+    while len(buf)>offset:
+      l,message_type= struct.unpack_from("!IB", buf, offset)
+      if l+offset>len(buf):  return
+      if message_type==self.ROBOT_MESSAGE:
+        self.UnpackRobotMessage(buf, offset, l)
+      elif message_type==self.ROBOT_STATE:
+        #self.UnpackRobotState(buf, offset, l)
+        pass
+      if message_type==self.PROGRAM_STATE_MESSAGE:
+        pass
+      offset+= l
+
+  def UnpackRobotMessage(self, buf, offset, l):
+    offset+= 5
+    timestamp,source,robot_message_type= struct.unpack_from("!Qbb", buf, offset)
+    offset+= 8+1+1
+    if robot_message_type==self.ROBOT_MESSAGE_VERSION:
+      self.version.timestamp= timestamp
+      self.version.source= source
+      self.version.robot_message_type= robot_message_type
+      self.version.project_name_size= struct.unpack_from("!b", buf, offset)[0]
+      offset+= 1
+      self.version.project_name= buf[offset:offset+self.version.project_name_size]
+      offset+= self.version.project_name_size
+      mj,mi,svn= struct.unpack_from("!bbI", buf, offset)
+      self.version.major_version,self.version.minor_version,self.version.svn_revision= mj,mi,svn
+      offset+= 1+1+4
+      self.version.build_date= buf[offset:l]
+      #if version_msg_.major_version<2:
+        #robot_mode_running_= ROBOT_RUNNING_MODE
+
+def GetURState(robot_hostname):
+  port= 30001
+  socketobj= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  socketobj.connect((robot_hostname, port))
+
+  robot_state= TURState()
+  buf= socketobj.recv(512)
+  socketobj.close()
+  robot_state.Unpack(buf)
+  return robot_state
+
+#Velocity control interface of UR.
+class TURVelCtrl(object):
+  def __init__(self, robot_hostname):
+    self.robot_hostname= robot_hostname
+
+  def Init(self):
+    self.robot_state= GetURState(self.robot_hostname)
+    self.robot_ver= self.robot_state.GetVersion()
+    port= 30003
+    #socketobj= socket.create_connection((robot_hostname, port))
+    self.socketobj= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.socketobj.connect((self.robot_hostname, port))
+
+  def Step(self, dq, acc):
+    if self.robot_ver>=3.3:
+      cmd= "speedj([%1.5f, %1.5f, %1.5f, %1.5f, %1.5f, %1.5f], %f, 0.008)\n" % (
+              dq[0],dq[1],dq[2],dq[3],dq[4],dq[5],acc)
+    elif self.robot_ver>=3.1:
+      cmd= "speedj([%1.5f, %1.5f, %1.5f, %1.5f, %1.5f, %1.5f], %f)\n" % (
+              dq[0],dq[1],dq[2],dq[3],dq[4],dq[5],acc)
+    else:
+      cmd= "speedj([%1.5f, %1.5f, %1.5f, %1.5f, %1.5f, %1.5f], %f, 0.02)\n" % (
+              dq[0],dq[1],dq[2],dq[3],dq[4],dq[5],acc)
+    self.AddCommandToQueue(cmd)
+
+  def Finish(self):
+    self.Step([0.0]*6, 10.0)
+    self.socketobj.close()
+
+  def AddCommandToQueue(self, cmd):
+    if cmd[-1]!='\n': cmd+= '\n'
+    len_sent= self.socketobj.send(cmd)
+    if len_sent!=len(cmd):
+      raise Exception('Command was not sent properly; cmd {cmd} byte, sent {sent} byte'.format(cmd=len(cmd),sent=len_sent))
+
 class TVelCtrl(object):
   #ct: core_tool.
   #rate: Control time cycle in Hz.
@@ -27,11 +149,9 @@ class TVelCtrl(object):
     self.q_limit_th= q_limit_th
     self.ct= ct
     self.arm= arm
-    ct.AddPub('trg_vel', '/ur_driver/joint_speed', trajectory_msgs.msg.JointTrajectory, queue_size=2)
+    self.velctrl= TURVelCtrl('ur3a2')  #FIXME:Get robot hostname from somewhere else
+    self.velctrl.Init()
     self.rate_adjuster= rospy.Rate(self.rate)
-    self.traj= trajectory_msgs.msg.JointTrajectory()
-    self.traj.joint_names= ct.robot.JointNames(arm)
-    self.traj.points= [trajectory_msgs.msg.JointTrajectoryPoint()]
     self.last_dq= [0.0]*ct.robot.DoF(self.arm)
 
   def Rate(self):
@@ -63,10 +183,9 @@ class TVelCtrl(object):
     if ddq_max>self.ddq_lim:
       scale= self.ddq_lim/ddq_max
       #print dq, dq0, [(v-v0)/dt for v,v0 in zip(dq,dq0)], scale
-      dqtmp= copy.deepcopy(dq)
       dq= [scale*v+(1.0-scale)*v0 for v,v0 in zip(dq,dq0)]
       CPrint(3,'ur.velctrl: dq is modified due to exceeding ddq_lim;',ddq_max,self.ddq_lim)
-      print scale,dq0,dqtmp,dq
+      #print scale,dq0,dqtmp,dq
     #Limit velocities:
     dq_max= max(map(abs,dq))
     if dq_max>self.dq_lim:
@@ -86,14 +205,12 @@ class TVelCtrl(object):
         dq[j]= (q2[j]-qi)/dt
         CPrint(3,'ur.velctrl: Joint angle is exceeding the limit;',qi,dqi,qmini,qmaxi)
 
-    self.traj.points[0].velocities= dq
-    self.traj.points[0].accelerations= [100.0]*dof
-
-    ct.pub.trg_vel.publish(self.traj)
+    self.velctrl.Step(dq, 100.0)
     self.last_dq= dq
+    print self.rate_adjuster.remaining().to_sec()
+    if self.rate_adjuster.remaining().to_sec()<0:
+      CPrint(4,'Loosing real-time control:', self.rate_adjuster.remaining().to_sec())
     if sleep:  self.rate_adjuster.sleep()
 
   def Finish(self):
-    ct= self.ct
-    self.traj.points[0].velocities= [0.0]*ct.robot.DoF(self.arm)
-    ct.pub.trg_vel.publish(self.traj)
+    self.velctrl.Finish()
