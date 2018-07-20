ead38a275ed0013588df459896540974745f2eae
Akihiko Yamaguchi <info@akihikoy.net>
2018-07-20 12:54:20 +0900

    Adjusted some FingerVision control parameters;

--- ay_skill/fv/ctrl_params.py
+++ ay_skill/fv/ctrl_params.py
@@ -22,8 +22,8 @@ def Run(ct,*args):
     elif ct.robot.EndEff(arm).Is('DxlGripper'):
       ct.GetAttr('fv_ctrl','min_gstep')[arm]= 0.002
       ct.GetAttr('fv_ctrl','effort')[arm]= 100.0
-      ct.SetAttr('fv_ctrl','pickup2a_gtimeout1', 20)  #Ctrl step=50Hz(Mikata), 500Hz(Bx)
-      ct.SetAttr('fv_ctrl','pickup2a_gtimeout2', 25)
+      ct.SetAttr('fv_ctrl','pickup2a_gtimeout1', 40)  #Ctrl step=50Hz(Mikata), 500Hz(Bx)
+      ct.SetAttr('fv_ctrl','pickup2a_gtimeout2', 50)
   if ct.robot.Is('Mikata'):
     ct.SetAttr('fv_ctrl','tracko_gain', [[0.1,0.1,0.1]])
     ct.SetAttr('fv_ctrl','pickup2a_kp', [1.0,1.0, 5.0,  1.0,1.0,1.0])
