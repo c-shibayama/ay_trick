a2c7760cec976eff98fc3d491ed978f3b8d73a39
Akihiko Yamaguchi <info@akihikoy.net>
2020-02-03 20:04:24 +0900

    Added utility scripts to use sensors:   aandd_weight: Weight scale of A and D co.   wrist_rs2: RealSense installed on a robot wrist;

---
+++ ay_skill/aandd_weight.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+from core_tool import *
+roslib.load_manifest('ay_util_msgs')
+import ay_util_msgs.msg
+
+def Help():
+  return '''A and D weight scale utility.
+  ay_util AandDEW_weight.py should be launched beforehand.
+  Usage:
+    aandd_weight 'on' [, NODE]
+    aandd_weight 'setup' [, NODE]
+      Start to subscribe the topics.
+      The observed data can be accessed via: ct.GetAttr(TMP,NODE)
+      NODE: Node name of the weight scale; default:weight.
+    aandd_weight
+    aandd_weight 'off' [, NODE]
+    aandd_weight 'clear' [, NODE]
+      Stop to subscribe the topics.
+      NODE: Node name of the weight scale; default:weight.
+  '''
+
+def ReceiveRaw(ct,l,msg):
+  l.raw= msg.data
+  l.stamp_raw= msg.header
+  if ct.callback.weight is not None:
+    ct.callback.weight('raw',l)
+
+def ReceiveValue(ct,l,msg):
+  l.value= msg.data
+  l.stamp_value= msg.header
+  if ct.callback.weight is not None:
+    ct.callback.weight('value',l)
+
+def Run(ct,*args):
+  if len(args)==0:
+    command= 'clear'
+  else:
+    command= args[0]
+    args= args[1:]
+
+  def UnSubscribe(node):
+    ct.DelSub('{node}_raw'.format(node=node))
+    ct.DelSub('{node}_value'.format(node=node))
+
+  if command in ('on','setup'):
+    node= args[0] if len(args)>0 else 'weight'
+
+    ct.SetAttr(TMP,node, TContainer(debug=True))
+    l= ct.GetAttr(TMP,node)
+    l.node= node
+    l.value= None  #Weight in gram.
+    l.stamp_value= None  #Stamp at the weight observation.
+    l.raw= None  #Raw reading.
+    l.stamp_raw= None  #Stamp at the raw observation.
+    ct.callback.weight= None
+
+    ct.AddSub('{node}_raw'.format(node=node), '/{node}/raw'.format(node=node), ay_util_msgs.msg.StringStamped, lambda msg,ct=ct,l=l:ReceiveRaw(ct,l,msg))
+    ct.AddSub('{node}_value'.format(node=node), '/{node}/value'.format(node=node), ay_util_msgs.msg.Float64Stamped, lambda msg,ct=ct,l=l:ReceiveValue(ct,l,msg))
+
+  elif command in ('off','clear'):
+    node= args[0] if len(args)>0 else 'weight'
+    UnSubscribe(node)
---
+++ ay_skill/wrist_rs2.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python
+from core_tool import *
+roslib.load_manifest('sensor_msgs')
+import cv2
+import sensor_msgs.msg
+from cv_bridge import CvBridge, CvBridgeError
+
+def Help():
+  return '''RealSense2 camera utility installed on the wrist of the robot arm.
+  Usage:
+    wrist_rs2 'on' [, OPTIONS]
+    wrist_rs2 'setup' [, OPTIONS]
+      Start to subscribe the topics.
+      The observed data can be accessed via: ct.GetAttr(TMP,RS_ATTR)
+      OPTIONS: Dictionary of options (default:DefaultOptions()).
+      RS_ATTR: OPTIONS['rs_attr']
+    wrist_rs2
+    wrist_rs2 'off' [, RS_ATTR]
+    wrist_rs2 'clear' [, RS_ATTR]
+      Stop to subscribe topics.
+    wrist_rs2 'show' [, RS_ATTR]
+      Display the received images.
+      RS_ATTR: Attribute name to access the data; default:'rs' (ct.GetAttr(TMP,RS_ATTR)).
+  '''
+
+def DefaultOptions():
+  return {
+    'types': ['depth'],  #List of subscribing components ('depth','rgb')
+    'arm': 0,  #Arm index of the robot on whose wrist the camera is installed.
+    'lx': [-0.035, 0.06, 0.0358, 0, 0, 0, 1],  #Pose of the camera in the wrist frame.
+    'rs_attr': 'rs',  #Captured data is saved into: ct.GetAttr(TMP,rs_attr)
+    }
+
+def GetCameraProjectionMatrix():
+  try:
+    cam_info= rospy.wait_for_message('/camera/aligned_depth_to_color/camera_info', sensor_msgs.msg.CameraInfo, 5.0)
+    proj_mat= np.array(cam_info.P).reshape(3,4) #get camera projection matrix from ros topic
+    return proj_mat
+  except (rospy.ROSException, rospy.ROSInterruptException):
+    raise Exception('Failed to read topic: /camera/aligned_depth_to_color/camera_info')
+
+def ReceiveDepth(ct,l,lh,msg):
+  with lh.thread_locker:
+    l.msg_depth= msg
+    l.stamp_depth= msg.header
+  try:
+    with lh.thread_locker:
+      #convert ros image to cv image (matrix) 16bit monochrome
+      l.img_depth= lh.cvbridge.imgmsg_to_cv2(msg, '16UC1')
+  except CvBridgeError as e:
+    print e
+  if ct.robot is not None:
+    with lh.thread_locker:
+      l.xw= ct.robot.FK(arm=l.arm)
+    ct.br.sendTransform(l.lw_x_camera_link[0:3],l.lw_x_camera_link[3:],
+        rospy.Time.now(), 'camera_link', ct.robot.EndLink(l.arm))
+  if ct.callback.rs is not None:
+    ct.callback.rs('depth',l,lh)
+
+def ReceiveRGB(ct,l,lh,msg):
+  with lh.thread_locker:
+    l.msg_rgb= msg
+    l.stamp_rgb= msg.header
+  try:
+    with lh.thread_locker:
+      l.img_rgb= lh.cvbridge.imgmsg_to_cv2(msg, 'bgr8')
+  except CvBridgeError as e:
+    print e
+  if ct.robot is not None and 'depth' not in l.options['types']:
+    with lh.thread_locker:
+      l.xw= ct.robot.FK(arm=l.arm)
+    ct.br.sendTransform(l.lw_x_camera_link[0:3],l.lw_x_camera_link[3:],
+        rospy.Time.now(), 'camera_link', ct.robot.EndLink(l.arm))
+  if ct.callback.rs is not None:
+    ct.callback.rs('rgb',l,lh)
+
+def Run(ct,*args):
+  if len(args)==0:
+    command= 'clear'
+  else:
+    command= args[0]
+    args= args[1:]
+
+  def UnSubscribe(rs_attr):
+    ct.DelSub('{rs_attr}_depth'.format(rs_attr=rs_attr))
+    ct.DelSub('{rs_attr}_rgb'.format(rs_attr=rs_attr))
+
+  if command in ('on','setup'):
+    user_options= args[0] if len(args)>0 else {}
+    options= DefaultOptions()
+    InsertDict(options, user_options)
+    UnSubscribe(options['rs_attr'])
+
+    ct.SetAttr(TMP,'{rs_attr}_helper'.format(rs_attr=options['rs_attr']), TContainer(debug=True))
+    lh= ct.GetAttr(TMP,'{rs_attr}_helper'.format(rs_attr=options['rs_attr']))
+    lh.cvbridge= CvBridge()
+    lh.thread_locker= threading.RLock()
+    ct.SetAttr(TMP,options['rs_attr'], TContainer(debug=True))
+    l= ct.GetAttr(TMP,options['rs_attr'])
+    l.options= options
+    l.proj_mat= GetCameraProjectionMatrix()
+    l.msg_depth= None  #Original depth message.
+    l.img_depth= None  #Depth image (for OpenCV).
+    l.stamp_depth= None  #Stamp of the depth message.
+    l.msg_rgb= None  #Original rgb message.
+    l.img_rgb= None  #RGB image (for OpenCV).
+    l.stamp_rgb= None  #Stamp of the rgb message.
+    ct.callback.rs= None
+    #Arm on which the RealSense is attached.
+    l.arm= options['arm']
+    #Pose of RealSense (camera_color_optical_frame) in the wrist frame.
+    l.lx= options['lx']
+    l.lw_x_camera_link= TransformRightInv(l.lx,ct.Run('tf_once','camera_link','camera_color_optical_frame'))
+    #Wrist pose at the observation. If both depth and rgb are observed, xw is measured only when depth is observed.
+    l.xw= None
+
+    if 'depth' in options['types']:
+      ct.AddSubW('{rs_attr}_depth'.format(rs_attr=options['rs_attr']),
+                 '/camera/aligned_depth_to_color/image_raw', sensor_msgs.msg.Image, lambda msg,ct=ct,l=l,lh=lh:ReceiveDepth(ct,l,lh,msg), time_out=3.0)
+    if 'rgb' in options['types']:
+      ct.AddSubW('{rs_attr}_rgb'.format(rs_attr=options['rs_attr']),
+                 '/camera/color/image_raw', sensor_msgs.msg.Image, lambda msg,ct=ct,l=l,lh=lh:ReceiveRGB(ct,l,lh,msg), time_out=3.0)
+
+  elif command in ('off','clear'):
+    rs_attr= args[0] if len(args)>0 else 'rs'
+    UnSubscribe(rs_attr)
+
+  elif command=='show':
+    rs_attr= args[0] if len(args)>0 else 'rs'
+    components= ct.GetAttr(TMP,rs_attr).options['types']
+    print 'Press any key to quit.'
+    kbhit= TKBHit()
+    try:
+      for c in components:
+        cv2.namedWindow(c)
+      while True:
+        if kbhit.IsActive():
+          key= kbhit.KBHit()
+          if key is not None:
+            break
+        else:
+          break
+        if 'depth' in components:  cv2.imshow('depth',ct.GetAttr(TMP,rs_attr).img_depth*255)
+        if 'rgb' in components:  cv2.imshow('rgb',ct.GetAttr(TMP,rs_attr).img_rgb)
+        key= cv2.waitKey(10)&0xFF
+        if key==ord('q'):
+          break
+    finally:
+      kbhit.Deactivate()
+      cv2.destroyAllWindows()
+      for _ in range(10):  cv2.waitKey(10)
+
