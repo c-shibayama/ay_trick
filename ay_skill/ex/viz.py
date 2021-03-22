fe4c65e950b0e4cc1ccc04bc74eaf6a768ac3951
Akihiko Yamaguchi <info@akihikoy.net>
2021-03-22 23:29:43 +0900

    Updated the sample of TSimpleVisualizer;

--- ay_skill/ex/viz.py
+++ ay_skill/ex/viz.py
@@ -12,9 +12,11 @@ def HelpKeyOp():
   h: Help.
   d: Delete all (DeleteAllMarkers).
   a: Add an arrow (AddArrow).
-  w: Add a list of arrows (AddArrowList).
+  A: Add a list of arrows (AddArrowList).
   c: Add a cube (AddCube).
+  C: Add a list of cubes (AddCubeList).
   s: Add a sphere (AddSphere).
+  S: Add a list of spheres (AddSphereList).
   y: Add a cylinder (AddCylinder).
   x: Add a cylinder (AddCylinderX).
   p: Add points (AddPoints).
@@ -23,6 +25,7 @@ def HelpKeyOp():
   0: Add a list of coordinate systems (AddArrowList).
   l: Add a polygon (AddPolygon).
   k: Add a list of lines (AddLineList).
+  t: Add a text (AddText).
   '''
 
 def Run(ct,*args):
@@ -52,16 +55,26 @@ def Run(ct,*args):
         elif key=='a':
           scale= (lambda a,b: [a,b,b])(np.random.uniform(0,0.5),np.random.uniform(0,0.01))
           mid= viz.AddArrow(x=RandX(), scale=scale, rgb=RandCol(), alpha=alpha, mid=mid)
-        elif key=='w':
+        elif key=='A':
           N= 10
           scale= [np.random.uniform(0,0.5),np.random.uniform(0,0.01)]
           mid= viz.AddArrowList(x_list=RandXL(N), scale=scale, rgb=RandColL(N), alpha=alpha, mid=mid)
         elif key=='c':
           scale= np.random.uniform(0,0.5,3)
           mid= viz.AddCube(x=RandX(), scale=scale, rgb=RandCol(), alpha=alpha, mid=mid)
+        elif key=='C':
+          N= 10
+          points= [RandP() for i in range(N)]
+          scale= np.random.uniform(0,0.5,3)
+          mid= viz.AddCubeList(points=points, scale=scale, rgb=RandColL(N), alpha=alpha, mid=mid)
         elif key=='s':
           scale= (lambda r: [r,r,r])(np.random.uniform(0,0.5))
           mid= viz.AddSphere(p=RandP(), scale=scale, rgb=RandCol(), alpha=alpha, mid=mid)
+        elif key=='S':
+          N= 10
+          points= [RandP() for i in range(N)]
+          scale= (lambda r: [r,r,r])(np.random.uniform(0,0.5))
+          mid= viz.AddSphereList(points=points, scale=scale, rgb=RandColL(N), alpha=alpha, mid=mid)
         elif key=='y':
           diameter= np.random.uniform(0,0.5)
           mid= viz.AddCylinder(p1=RandP(), p2=RandP(), diameter=diameter, rgb=RandCol(), alpha=alpha, mid=mid)
@@ -98,6 +111,10 @@ def Run(ct,*args):
           points= np.random.uniform(-0.5,0.5,(N,3)).tolist()
           scale= np.random.uniform(0,0.1,1)
           mid= viz.AddLineList(points, scale=scale, rgb=RandColL(N), alpha=alpha, mid=mid)
+        elif key=='t':
+          text= ''.join([chr(np.random.randint(ord('A'),ord('z')+1)) for _ in range(np.random.randint(20))])
+          scale= np.random.uniform(0,0.1,1)
+          mid= viz.AddText(p=RandP(), text=text, scale=scale, rgb=RandCol(), alpha=alpha, mid=mid)
       else:  break
       rospy.sleep(200.0e-3)
 
